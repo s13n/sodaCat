df9885c8afcf4a851f764acd57c82669c25b3075
Stefan Heinzmann <heinzmann@schoeps.de>
2026-01-21 17:38:20 +0100

    Add missing braces

--- generators/cxx/generate_clocktree_header.py
+++ generators/cxx/generate_clocktree_header.py
@@ -175,18 +175,18 @@ def formatFields(field_list, instance):
             }}'''
         elif values:
             f_get = f'''[](void const *ctx) -> uint32_t {{
-                constexpr uint32_t const values[] = {",".join(str(x) for x in values)};
+                constexpr uint32_t values[] = {{ {",".join(str(x) for x in values)} }};
                 auto idx = i_{inst}.registers->{reg}.get().{field};
                 return idx < std::size(values) ? values[idx] : 0;
             }}'''
             f_set = f'''[](void *ctx, uint32_t val){{
-                constexpr uint32_t const values[] = {",".join(str(x) for x in values)};
+                constexpr uint32_t values[] = {{ {",".join(str(x) for x in values)} }};
                 auto reg = i_{inst}.registers->{reg}.get();
                 for (size_t i=0; i<std::size(values); ++i)
-                    if (values[i] == val) {
+                    if (values[i] == val) {{
                         reg.{field} = i;
                         break;
-                    }
+                    }}
                 i_{inst}.registers->{reg}.set(reg);
             }}'''
         else:
